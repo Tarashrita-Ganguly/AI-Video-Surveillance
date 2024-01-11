780ead73ceaff3a3380df5206d811e0741a1164a
Tarashrita Ganguly <150145692+Tarashrita-Ganguly@users.noreply.github.com>
2024-01-11 18:34:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
+video="";
+
+function preload(){
+    video=createVideo('video.mp4');
+    video.hide();
+}
+
 function setup() {
-    canvas = createCanvas(380,380);
+    canvas = createCanvas(480,380);
     canvas.center();
 
+}
+
+function draw(){
+    image(video,0,0,480,380);
+}
+function start() {
+    document.getElementById("status").innerHTML = "Status: Detecting objects";
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+}
+
+function modelLoaded(){
+    console.log("model loaded!");
+    status=true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
 }
